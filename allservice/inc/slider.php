a163c636cc05751f1ffe262d319096e434b89365
Mohammad Didarul Alam <emdidar@gmail.com>
2018-07-03 00:59:38 +0600

    update

--- allservice/inc/slider.php
+++ allservice/inc/slider.php
@@ -13,31 +13,31 @@
         <!-- Wrapper for slides -->
         <div class="carousel-inner" role="listbox">
         <div class="item active">
-          <img src="images/bg6.jpg" alt="Image">
+          <img src="images/slide1.jpg" alt="Image">
           <div class="carousel-caption">
            <div class="storename">
              <h3 style="font-size:50px; border: 3px solid white; border-radius: 15px; position: relative; top: 0.5px; width: 500px; margin: 0 auto; background-color: black; opacity: 0.80; font-color: white; font-weight: 600; text-align: center; font-family: palatino">ALLSERVICE.COM</h3>
-             <p style="font-size:20px">The Name of Exellence</p>
+             <p style="font-size:20px; color:gray">The Name of Exellence</p>
            </div>
           </div>      
         </div>
 
         <div class="item">
-          <img src="images/bg6.jpg" alt="Image">
+          <img src="images/slide2.jpg" alt="Image">
           <div class="carousel-caption" >
            <div class="storename">
              <h3 style="font-size:50px; border: 3px solid white; border-radius: 15px; position: relative; top: 0.5px; width: 500px; margin: 0 auto; background-color: black; opacity: 0.80; font-color: white; font-weight: 600; text-align: center; font-family: palatino">ALLSERVICE.COM</h3>
-             <p style="font-size:20px">The Name of Exellence</p>
+             <p style="font-size:20px; color:gray">The Name of Exellence</p>
            </div>
           </div>      
         </div>
 
         <div class="item">
-          <img src="images/bg6.jpg" alt="Image">
+          <img src="images/slide3.jpg" alt="Image">
           <div class="carousel-caption">
            <div class="storename">
              <h3 style="font-size:50px; border: 3px solid white; border-radius: 15px; position: relative; top: 0.5px; width: 500px; margin: 0 auto; background-color: black; opacity: 0.80; font-color: white; font-weight: 600; text-align: center; font-family: palatino">ALLSERVICE.COM</h3>
-             <p style="font-size:20px">The Name of Exellence</p>
+             <p style="font-size:20px; color:gray">The Name of Exellence</p>
            </div>
           </div>      
         </div>
