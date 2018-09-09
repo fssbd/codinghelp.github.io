3d7f7b0c69928cbf9e44d0c08843fa15917549e6
Mohammad Didarul Alam <emdidar@gmail.com>
2018-09-09 09:47:26 +0600

    dbFile

--- paybd/DB File/codinghe_paybd.sql
+++ paybd/DB File/codinghe_paybd.sql
@@ -11,11 +11,13 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 -- Dumping database structure for codinghe_paybd
+DROP DATABASE IF EXISTS `codinghe_paybd`;
 CREATE DATABASE IF NOT EXISTS `codinghe_paybd` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `codinghe_paybd`;
 
 
 -- Dumping structure for table codinghe_paybd.tbcompanyinfo
+DROP TABLE IF EXISTS `tbcompanyinfo`;
 CREATE TABLE IF NOT EXISTS `tbcompanyinfo` (
   `autoId` int(11) NOT NULL AUTO_INCREMENT,
   `companyName` varchar(150) NOT NULL DEFAULT '',
@@ -33,7 +35,24 @@ INSERT INTO `tbcompanyinfo` (`autoId`, `companyName`, `email`, `mobile`, `office
 /*!40000 ALTER TABLE `tbcompanyinfo` ENABLE KEYS */;
 
 
+-- Dumping structure for table codinghe_paybd.tbcurrencyinfo
+DROP TABLE IF EXISTS `tbcurrencyinfo`;
+CREATE TABLE IF NOT EXISTS `tbcurrencyinfo` (
+  `autoId` int(11) NOT NULL AUTO_INCREMENT,
+  `currencyName` varchar(100) NOT NULL,
+  `isoCode` varchar(100) NOT NULL,
+  `addOrLess` double NOT NULL,
+  PRIMARY KEY (`autoId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='select autoId,currencyName,isoCode,addOrLess from tbCurrencyInfo';
+
+-- Dumping data for table codinghe_paybd.tbcurrencyinfo: ~0 rows (approximately)
+DELETE FROM `tbcurrencyinfo`;
+/*!40000 ALTER TABLE `tbcurrencyinfo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbcurrencyinfo` ENABLE KEYS */;
+
+
 -- Dumping structure for table codinghe_paybd.tbl_admin
+DROP TABLE IF EXISTS `tbl_admin`;
 CREATE TABLE IF NOT EXISTS `tbl_admin` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
@@ -50,11 +69,12 @@ INSERT INTO `tbl_admin` (`id`, `username`, `password`) VALUES
 
 
 -- Dumping structure for table codinghe_paybd.tbnoticeinfo
+DROP TABLE IF EXISTS `tbnoticeinfo`;
 CREATE TABLE IF NOT EXISTS `tbnoticeinfo` (
   `autoId` int(11) NOT NULL AUTO_INCREMENT,
   `noticeName` varchar(150) DEFAULT NULL,
   PRIMARY KEY (`autoId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COMMENT='select youtube,facebook,twiter,instagram from tbSocialInfo';
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COMMENT='select autoId,noticeName from tbnoticeinfo';
 
 -- Dumping data for table codinghe_paybd.tbnoticeinfo: ~0 rows (approximately)
 DELETE FROM `tbnoticeinfo`;
@@ -64,7 +84,28 @@ INSERT INTO `tbnoticeinfo` (`autoId`, `noticeName`) VALUES
 /*!40000 ALTER TABLE `tbnoticeinfo` ENABLE KEYS */;
 
 
+-- Dumping structure for table codinghe_paybd.tbsestimonials
+DROP TABLE IF EXISTS `tbsestimonials`;
+CREATE TABLE IF NOT EXISTS `tbsestimonials` (
+  `autoId` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `comment` varchar(350) NOT NULL,
+  `image` longblob NOT NULL,
+  PRIMARY KEY (`autoId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COMMENT='select autoId,userId,,comment,image from tbSestimonials';
+
+-- Dumping data for table codinghe_paybd.tbsestimonials: ~2 rows (approximately)
+DELETE FROM `tbsestimonials`;
+/*!40000 ALTER TABLE `tbsestimonials` DISABLE KEYS */;
+INSERT INTO `tbsestimonials` (`autoId`, `userId`, `comment`, `image`) VALUES
+	(1, 1, 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate ', _binary 0xFE00000000),
+	(2, 2, '\r\n100% ????????? ????? ??? ??? ???? ????? ??? ??? ?? ??? ??????? ?? ??? ?? ????? ??? ??, ???? ????? ?????? ??? ???? ??????', _binary 0xFE002E0000),
+	(3, 3, 'Could I... BE any more happy with this company?', _binary 0xFE00000000);
+/*!40000 ALTER TABLE `tbsestimonials` ENABLE KEYS */;
+
+
 -- Dumping structure for table codinghe_paybd.tbsocialinfo
+DROP TABLE IF EXISTS `tbsocialinfo`;
 CREATE TABLE IF NOT EXISTS `tbsocialinfo` (
   `autoId` int(11) NOT NULL AUTO_INCREMENT,
   `youtube` varchar(150) DEFAULT NULL,
@@ -74,7 +115,7 @@ CREATE TABLE IF NOT EXISTS `tbsocialinfo` (
   PRIMARY KEY (`autoId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COMMENT='select youtube,facebook,twiter,instagram from tbSocialInfo';
 
--- Dumping data for table codinghe_paybd.tbsocialinfo: ~1 rows (approximately)
+-- Dumping data for table codinghe_paybd.tbsocialinfo: ~0 rows (approximately)
 DELETE FROM `tbsocialinfo`;
 /*!40000 ALTER TABLE `tbsocialinfo` DISABLE KEYS */;
 INSERT INTO `tbsocialinfo` (`autoId`, `youtube`, `facebook`, `twiter`, `instagram`) VALUES
